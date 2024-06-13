48c59c6a68ae37e88e64b675691815350c1a3168
labazov13 <labazopelastra@gmail.com>
2024-06-13 14:40:49 +0700

    End 2-nd example

--- src/main/java/com/example/SpringSecurityJWT/model/Person.java
+++ src/main/java/com/example/SpringSecurityJWT/model/Person.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
